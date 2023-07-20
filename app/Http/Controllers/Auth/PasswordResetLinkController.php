f081b3680b49025ca0876f659390f21ef502285c
Vinod Sarvode <vs10201020@gmail.com>
2023-07-20 19:05:58 +0530

    sm

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\WelcomeNotification;
+use App\Models\User;
+use Mail;
 
 class PasswordResetLinkController extends Controller
 {
@@ -17,6 +21,8 @@ class PasswordResetLinkController extends Controller
     {
         return view('auth.forgot-password');
     }
+
+   
 
     /**
      * Handle an incoming password reset link request.
@@ -29,12 +35,19 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
+        $user = User::where('email', $request->email)->first();
+
+        if($user){
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+            Notification::sendNow($user, new WelcomeNotification());
+        }
+
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+       
 
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
